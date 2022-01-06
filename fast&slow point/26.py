f66f37572fc8c6eb3ef8d0a52e9371cdf03cb053
Sidi Yang <79249381+Stephen0808@users.noreply.github.com>
2022-01-06 21:29:26 +0800

    Create 26.py

---
+++ fast&slow point/26.py
@@ -0,0 +1,8 @@
+def removeDuplicates(self, nums: List[int]) -> int:
+        fast, slow = 0 ,0
+        while(fast < len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1
